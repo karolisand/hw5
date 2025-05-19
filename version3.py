44f659607b3729e6b43fb492e9e887a04a3b3ffc
karolisand <karolis.andrikonis@gmail.com>
2025-05-19 17:49:47 +0300

    rich table done

--- version3.py
+++ version3.py
@@ -1,43 +1,40 @@
 
 from rich.console import Console
 from rich.table import Table
+import re
 
 console = Console()
-table = Table(title="ToDo list")
-
 todo_list = []
 
 while True:
     todo_title = input("\nPlease enter ToDo title: ")
     todo_description = input("Please enter ToDo description: ")
-
-    repeat = True
     
-    while repeat:
-        import re
-
-        pattern = r"\d{4}-\d{2}-\d{2}"                                           # Set format: xxxx-xx-xx
+    while True:
         todo_data = input("Please ad ToDo date (in format: yyyy-mm-dd): ")
-
+        pattern = r"\d{4}-\d{2}-\d{2}"                                           # Set format: xxxx-xx-xx
+    
         if not re.fullmatch(pattern, todo_data):
             console.print("\nYou entered data in a wrong format.", style="red")
         else:
-            repeat = False
+            break
+            # repeat = False
 
     todo = {
         'title' : todo_title,
         'description' : todo_description,
-        'data' : todo_data
+        'date' : todo_data
     }
 
     todo_list.append(todo)
 
-    console.print("\nToDo list:\n", style="bold cyan")
+    table = Table(title="ToDo list")
+    table.add_column("ToDo Title", style="bold")
+    table.add_column("ToDo Description")
+    table.add_column("ToDo Due Date", style="green")
 
-    for p in todo_list:
+    for item in todo_list:
+        table.add_row(item['title'], item['description'], item['date'])
 
-
-        
-        console.print(f"ToDo Title: [magenta]{p['title']}[/]")
-        console.print(f"ToDo Description: [magenta]{p['description']}[/]")
-        console.print(f"ToDo Due Date: [magenta]{p['data']}[/]")
+    console.print(table)
+    
